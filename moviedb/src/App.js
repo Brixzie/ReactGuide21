39690ad7540de08220a9c4b6122647a29e5eff78
Alexander Brix <brixzen@hotmail.com>
2021-11-20 12:27:41 +0100

    new Version

---
+++ moviedb/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Lamp from './Lamp';
+import LightSwitch from './LightSwitch';
+
+//const Star = () => React.createElement('div', null, 'This is a little star')
+
+const Room = styled.div`
+  position: relative;
+  width: 500px;
+  height: 500px;
+  border: 10px solid black;
+  margin: 0 auto;
+`;
+
+function App() {
+
+  const [isLampOneOn, setIsLampOneOn] = useState(false);
+  const [isLampTwoOn, setIsLampTwoOn] = useState(false);
+  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
+  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
+
+  
+
+  return (
+    <Room>
+    <Lamp lampOn={isLampOneOn} position='left' />
+    <Lamp lampOn={isLampTwoOn} position='right' />
+    <LightSwitch
+      name='one'
+      callback={handleLightSwitchOne}
+      switchOn={isLampOneOn}
+      position='left'
+    />
+    <LightSwitch
+      name='two'
+      callback={handleLightSwitchTwo}
+      switchOn={isLampTwoOn}
+      position='right'
+    />
+  </Room>
+  );
+}
+
+export default App;
