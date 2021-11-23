53113bfdc38027407c6273f5f9e28f090f26aa52
Alexander Brix <brixzen@hotmail.com>
2021-11-23 17:08:55 +0100

    first commit

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,44 +1,24 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import Lamp from './Lamp';
-import LightSwitch from './LightSwitch';
+
+//Components
+import Header from './components/Header';
+import Home from './components/Home';
+
+//styles
+import { GlobalStyle } from './GlobalStyle'
+
 
 //const Star = () => React.createElement('div', null, 'This is a little star')
 
-const Room = styled.div`
-  position: relative;
-  width: 500px;
-  height: 500px;
-  border: 10px solid black;
-  margin: 0 auto;
-`;
+
 
 function App() {
-
-  const [isLampOneOn, setIsLampOneOn] = useState(false);
-  const [isLampTwoOn, setIsLampTwoOn] = useState(false);
-  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
-  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
-
-  
-
   return (
-    <Room>
-    <Lamp lampOn={isLampOneOn} position='left' />
-    <Lamp lampOn={isLampTwoOn} position='right' />
-    <LightSwitch
-      name='one'
-      callback={handleLightSwitchOne}
-      switchOn={isLampOneOn}
-      position='left'
-    />
-    <LightSwitch
-      name='two'
-      callback={handleLightSwitchTwo}
-      switchOn={isLampTwoOn}
-      position='right'
-    />
-  </Room>
+    <div className="App">
+      <Header />
+      <Home />
+      <GlobalStyle />
+    </div>
   );
 }
 
---
+++ moviedb/src/components/Home.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+
+//API
+import API from '../API'
+
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
+//components
+
+//Hook
+
+//Image
+import NoImage from '../images/no_image.jpg'
+
+const Home = () => {
+    const [state, setState] = useState();
+    const [loading, setLoading] = useState(false); 
+    const [error, setError] = useState(false);
+
+    //searchTerm default set to ""
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            //keep track of when we're loading so we can use things like loader icons
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            //console.log(movies);
+
+            //returns an object
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }))
+
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+    //Initial render
+    //(dependency array (what's it dependend on to trigger))
+    useEffect(() => {
+        fetchMovies(1)
+    }, [])
+
+    console.log(state);
+
+    return <div>Home Page</div>
+}
+
+export default Home;
